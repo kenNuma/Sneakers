db6c8c869dc2a460f9eb732f77810e59ffb90d7f
kamura.f <y-music.like2@ezweb.ne.jp>
2025-12-04 15:37:11 +0900

    完成

--- js/main.js
+++ js/main.js
@@ -2,13 +2,31 @@ $(function () {
   /*=================================================
   ハンバーガーメニュー
   ===================================================*/
+  $(".hunberger").on("click", function() {
+    $("header").toggleClass("open");
+  })
 
+  $(".mask").on("click", function() {
+    $("header").toggleClass("open");
+  })
 
   /*=================================================
   スムーススクロール
   ===================================================*/
-  
-  
+  $('a[href^="#"]').click(function () {
+    // クリックしたaタグのリンクを取得
+    let href = $(this).attr("href");
+    // ジャンプ先のid名をセット hrefの中身が#もしくは空欄なら,htmlタグをセット
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ページトップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う   ページトップからpositionだけスクロールする
+    // 600はスクロール速度で単位はミリ秒  swingはイージングのひとつ
+    $("html, body").animate({ scrollTop: position }, 600, "swing");
+    $("header").toggleClass("open");
+    // urlが変化しないようにfalseを返す
+    return false; 
+  });
   /*=================================================
   PICK UP スライダー
   ===================================================*/
@@ -26,7 +44,7 @@ $(function () {
         breakpoint: 768,
         settings: {
           centerPadding: "50px",
-          slidesToShow: 1500,
+          slidesToShow: 1,
         },
       },
     ],
@@ -38,7 +56,7 @@ $(function () {
   // スクロール時のイベント
   $(window).scroll(function () {
     // fadeinクラスに対して順に処理を行う
-    $("").each(function () {
+    $(".fadein").each(function () {
       // スクロールした距離
       let scroll = $(window).scrollTop();
       // fadeinクラスの要素までの距離
@@ -48,8 +66,8 @@ $(function () {
       // fadeinクラスの要素が画面下にきてから200px通過した
       // したタイミングで要素を表示
       if (scroll > target - windowHeight + 200) {
-        $(this).css("opacity", "");
-        $(this).css("transform", "translateY()");
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
       }
     });
   });
